a98993c46bbc7edb803d41404e2193ee8dfc485c
aiien61 <aiien61will@gmail.com>
2023-10-22 16:44:42 +0800

    tracking index

--- nditer.py
+++ nditer.py
@@ -44,6 +44,10 @@ with np.nditer(a, op_flags=['readwrite']) as it:
     for x in it:
         x[...] = 2 * x
 print(a)
+with np.nditer(a, op_flags=['readwrite']) as it:
+    for x in it:
+        x[...] = x / 2
+print(a)
 
 print('================== external loop')
 print(a)
@@ -53,3 +57,66 @@ print()
 
 for i in np.nditer(a, flags=['external_loop'], order='F'):
     print(i)
+
+print('================== tracking index')
+print(a)
+it = np.nditer(a, flags=['f_index'])
+for x in it:
+    print(f"{x} <{it.index}>")
+print()
+
+it = np.nditer(a, flags=['multi_index'])
+for x in it:
+    print(f"{x} <{it.multi_index}>")
+print()
+
+with np.nditer(a, flags=['multi_index'], op_flags=['writeonly']) as it:
+    for x in it:
+        x[...] = it.multi_index[1] - it.multi_index[0]
+print(a)
+
+print('================== tracking index by checking whether finished or not')
+a = np.arange(6).reshape(2,3)
+print(a)
+it = np.nditer(a, flags=['f_index'])
+while not it.finished:
+    print(f"{it[0]} <{it.index}>")
+    is_not_finished = it.iternext()
+print()
+
+it = np.nditer(a, flags=['multi_index'])
+while not it.finished:
+    print(f"{it[0]} <{it.multi_index}>")
+    is_not_finished = it.iternext()
+print()
+
+with np.nditer(a, flags=['multi_index'], op_flags=['writeonly']) as it:
+    while not it.finished:
+        it[0] = it.multi_index[1] - it.multi_index[0]
+        is_not_finished = it.iternext()
+print(a)
+
+print('================== one dimension')
+a = np.arange(6)
+print(a)
+it = np.nditer(a, flags=['f_index'])
+for x in it:
+    print(f"{x} <{it.index}>")
+print()
+
+it = np.nditer(a, flags=['multi_index'])
+for x in it:
+    print(f"{x} <{it.multi_index}>")
+print()
+
+it = np.nditer(a, flags=['f_index'])
+while not it.finished:
+    print(f"{it[0]} <{it.index}>")
+    is_not_finished = it.iternext()
+print()
+
+it = np.nditer(a, flags=['multi_index'])
+while not it.finished:
+    print(f"{it[0]} <{it.multi_index}>")
+    is_not_finished = it.iternext()
+print()
