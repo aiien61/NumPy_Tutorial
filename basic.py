703969be2707ba7f43dc57787a8d9fc0c423b1b7
aiien61 <aiien61will@gmail.com>
2023-11-01 14:04:48 +0800

    reorganise reshape examples

--- basic.py
+++ basic.py
@@ -10,33 +10,6 @@ def three_dimensional_array():
     pprint(d)
     print(d.shape)
 
-def reshape():
-    c = np.arange(1, 7).reshape(2, 3)
-    d = np.array([[[1,2,3], [4,5,6], [7,8,9], [10,11,12]],
-                  [[13,14,15], [16,17,18], [19,20,21], [22,23,24]]])
-    
-    pprint(c)
-    print(c.shape)
-
-    c = c.reshape(3, 2)
-    pprint(c)
-    print(c.shape)
-
-    c = c.reshape(6, 1)
-    pprint(c)
-    print(c.shape)
-
-    pprint(d)
-    print(d.shape)
-
-    d = d.reshape(2, 6, 2)
-    pprint(d)
-    print(d.shape)
-
-    d = d.reshape(8, 3)
-    pprint(d)
-    print(d.shape)
-
 
 def calculate_time():
     a = np.random.randn(100000)
